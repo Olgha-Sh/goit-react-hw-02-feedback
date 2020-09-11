2b7e73f7037b1650aea316b87a215ffb3057b860
Olia <olga.shostak1991@gmail.com>
2020-09-11 13:52:11 +0300

    Updates

---
+++ precache-manifest.89025cdf352e7db854588308a487e009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "058062c58e5a68cba73579f44c1ae70a",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "da8d3a0d2fa6cb024d42",
+    "url": "/goit-react-hw-02-feedback/static/css/main.701fc295.chunk.css"
+  },
+  {
+    "revision": "f4cf7324ff7b2c33affe",
+    "url": "/goit-react-hw-02-feedback/static/js/2.3729890f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-02-feedback/static/js/2.3729890f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da8d3a0d2fa6cb024d42",
+    "url": "/goit-react-hw-02-feedback/static/js/main.348362e7.chunk.js"
+  },
+  {
+    "revision": "75552aea3414b74608ba",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.569f9452.js"
+  }
+]);
